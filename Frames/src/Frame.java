4837174ac75932462cd4d173b9c1c7551463e8b2
Daniel E. Torres <42423604+DanielTorres10@users.noreply.github.com>
2018-09-03 18:27:52 -0400

    Alien figure

---
+++ Frames/src/Frame.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class Frame 
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		
+		frame.setSize(500,600);
+		frame.setTitle("Alien");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Art component = new Art();
+		frame.add(component);
+		
+		frame.setVisible(true);
+	}
+
+}
